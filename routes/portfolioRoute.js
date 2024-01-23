a65e4aaeafb7e29d1b4dbb42986abcbe08ac37f2
OmarD <omar.daghestani@colorado.edu>
2024-01-23 12:30:57 -0700

    reset my work, start time is 3:29 in video

--- routes/portfolioRoute.js
+++ routes/portfolioRoute.js
@@ -1,32 +1,32 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const {
   Intro,
   About,
-  Experience,
   Projects,
-  Education,
   Contact,
+  Experience,
+  Education,
 } = require("../models/portfolioModel");
 
 router.get("/get-portfolio-data", async (req, res) => {
   try {
     const intros = await Intro.find();
     const abouts = await About.find();
-    const experiences = await Experience.find();
     const projects = await Projects.find();
-    const educations = await Education.find();
     const contacts = await Contact.find();
+    const experiences = await Experience.find();
+    const educations = await Education.find();
 
     res.status(200).send({
       intro: intros[0],
       about: abouts[0],
       projects: projects,
       contact: contacts[0],
-      experience: experiences,
+      experiences: experiences,
       education: educations,
     });
   } catch (error) {
-    res._construct(500).send(error);
+    res.status(500).send(error);
   }
 });
 
